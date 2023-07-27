d1194020d14dd5d856fa960df4fb8aa2d3461d89
Carlos Eduardo Lima <carloseduardolima.dev@gmail.com>
2023-07-27 14:57:48 -0300

    Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 'use client'
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
+import TripSearch from './components/TripSearch';
 
 
 export default function Home() {
   const { data } = useSession()
 
   return (
-    <div>
-      <div  className='flex flex-col'>
-      <button type="button" onClick={() => signIn("google")}>Login</button>
-      <button type="button" onClick={() => signOut()}>Sair</button>
-      </div>
-      <h1>Olá, {data?.user?.name}</h1>
-      <img className='rounded-full' height={100} width={100} src={data?.user?.image ?? ""} alt='imagem do usuário' />
+    <div>      
+      <TripSearch/>
     </div>
   )
+
 }
