3c4a419d59663019d9db8b7416993f869385f1f2
Carlos Eduardo Lima <carloseduardolima.dev@gmail.com>
2023-07-27 15:11:28 -0300

    feat: add button component

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -4,10 +4,11 @@ import React from 'react'
 import CurrencyInput from '@/components/CurrencyInput'
 import DatePicker from '@/components/DatePicker'
 import Input from '@/components/Input'
+import Button from '@/components/Button'
 
 const TripSearch = () => {
     return (
-        <div className='container mx-auto p-5'>
+        <div className='container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat'>
             <h1 className='font-semibold text-2xl text-center text-primaryDarker'>Encontre sua próxima <span className='text-primary'>viagem!</span>
             </h1>
 
@@ -18,6 +19,7 @@ const TripSearch = () => {
                     <DatePicker className='w-full' placeholderText='Data de ida' onChange={()=> {}}/>
                     <CurrencyInput placeholder='Orçamento'/>                
                 </div>
+                <Button>Buscar</Button>
 
             </div>
 
